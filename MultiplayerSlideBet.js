27f6e5d335327030e3d92c0cc6fa6add045906d2
Avinav Aery <aery1803@gmail.com>
2025-01-16 15:19:56 +0530

    Initial Commit

---
+++ MultiplayerSlideBet.js
@@ -0,0 +1,212 @@
+const identifier = "";
+
+const generateRandomArrays = ({ length, min = 1.01, max }) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(Number((Math.random() * (max - min) + min).toFixed(6)));
+  }
+  return arr;
+};
+
+const multipliers = [
+  ...generateRandomArrays({ length: 160, max: 2 }),
+  ...generateRandomArrays({ length: 100, max: 5 }),
+  ...generateRandomArrays({ length: 40, max: 20 }),
+  ...generateRandomArrays({ length: 25, max: 100 }),
+  ...generateRandomArrays({ length: 15, max: 3000 }),
+  ...generateRandomArrays({ length: 10, max: 100000 }),
+];
+
+const amountPerBet = [
+  ...generateRandomArrays({ length: 250, min: 0.1, max: 0.5 }),
+  ...generateRandomArrays({ length: 100, min: 0.05, max: 0.25 }),
+];
+
+let betPlayed = [];
+let totalBetPlayed = [];
+
+const executeBets = async () => {
+  await Promise.all(
+    multipliers.map(
+      (cashoutAt, index) =>
+        new Promise((resolve) =>
+          setTimeout(() => {
+            fetch("https://stake.ac/_api/graphql", {
+              headers: {
+                accept: "*/*",
+                "accept-language": "en-US,en;q=0.5",
+                "content-type": "application/json",
+                priority: "u=1, i",
+                "sec-ch-ua":
+                  '"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+                "sec-ch-ua-arch": '"x86"',
+                "sec-ch-ua-bitness": '"64"',
+                "sec-ch-ua-full-version-list":
+                  '"Brave";v="131.0.0.0", "Chromium";v="131.0.0.0", "Not_A Brand";v="24.0.0.0"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-model": '""',
+                "sec-ch-ua-platform": '"Windows"',
+                "sec-ch-ua-platform-version": '"10.0.0"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "sec-gpc": "1",
+                "x-access-token":
+                  "4ef5ed7fb67e30e1050e24779114cfdbd52cda62d922e1cd08de4b6a34d77349e1b95b2b53a3796e12fefa0cf79509b7",
+                "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+              },
+              referrer: "https://stake.ac/casino/games/slide",
+              referrerPolicy: "strict-origin-when-cross-origin",
+              method: "POST",
+              mode: "cors",
+              credentials: "include",
+              body: JSON.stringify({
+                query:
+                  "mutation MultiplayerSlideBet($amount: Float!, $currency: CurrencyEnum!, $cashoutAt: Float!, $identifier: String!) {\n  multiplayerSlideBet(\n    amount: $amount\n    currency: $currency\n    cashoutAt: $cashoutAt\n    identifier: $identifier\n  ) {\n    __typename\n    ...MultiplayerSlideBet\n    user {\n      id\n      activeSlideBet {\n        ...MultiplayerSlideBet\n      }\n    }\n  }\n}\n\nfragment MultiplayerSlideBet on MultiplayerSlideBet {\n  id\n  user {\n    id\n    name\n    preferenceHideBets\n  }\n  payoutMultiplier\n  gameId\n  amount\n  payout\n  currency\n  slideResult: result\n  updatedAt\n  cashoutAt\n  btcAmount: amount(currency: btc)\n  active\n  createdAt\n}",
+                variables: {
+                  amount: amountPerBet?.[index],
+                  identifier: identifier,
+                  currency: "inr",
+                  cashoutAt: cashoutAt,
+                },
+              }),
+            }).then((res) => {
+              res
+                .json()
+                .then((result) => {
+                  const data = result?.data?.multiplayerSlideBet || {};
+                  if (data?.cashoutAt && data?.amount) {
+                    betPlayed.push({
+                      multiplier: data?.cashoutAt,
+                      amount: data?.amount,
+                    });
+                  }
+                })
+                .catch(() => null)
+                .finally(() => resolve());
+              1;
+            });
+          }, index * 50)
+        )
+    )
+  );
+};
+
+const fetchStats = async () => {
+  await new Promise((resolve) =>
+    fetch("https://stake.ac/_api/graphql", {
+      headers: {
+        accept: "application/graphql+json, application/json",
+        "accept-language": "en-US,en;q=0.5",
+        "content-type": "application/json",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-arch": '"x86"',
+        "sec-ch-ua-bitness": '"64"',
+        "sec-ch-ua-full-version-list":
+          '"Brave";v="131.0.0.0", "Chromium";v="131.0.0.0", "Not_A Brand";v="24.0.0.0"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-model": '""',
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-ch-ua-platform-version": '"10.0.0"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "sec-gpc": "1",
+        "x-access-token":
+          "4ef5ed7fb67e30e1050e24779114cfdbd52cda62d922e1cd08de4b6a34d77349e1b95b2b53a3796e12fefa0cf79509b7",
+        "x-language": "en",
+      },
+      referrer: "https://stake.ac/casino/games/slide",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      body: '{"query":"query SlideGameListHistory($limit: Int, $offset: Int) {\\n  slideGameList(limit: $limit, offset: $offset) {\\n    id\\n    multiplier\\n    startTime\\n    hash {\\n      hash\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n","operationName":"SlideGameListHistory","variables":{}}',
+    })
+      .then((res) => res.json().catch(() => null))
+      .then((result) => {
+        const data = result?.data?.slideGameList?.[0] || {};
+        const winningMultiplier = data?.multiplier || 0;
+        const calculateDetails = {
+          multiplier: winningMultiplier,
+          totalBets: betPlayed.length,
+          ...betPlayed?.reduce(
+            (accumulator, { multiplier, amount }) => {
+              return {
+                ...accumulator,
+                totalAmount: accumulator.totalAmount + (amount || 0),
+                winningBets:
+                  accumulator.winningBets +
+                  (multiplier < winningMultiplier ? 1 : 0),
+                winningAmount:
+                  accumulator.winningAmount +
+                  (multiplier < winningMultiplier ? amount * multiplier : 0),
+              };
+            },
+            { winningBets: 0, winningAmount: 0, totalAmount: 0 }
+          ),
+        };
+        const updatedDetails = {
+          ...calculateDetails,
+          multiplier: Number(calculateDetails.multiplier.toFixed(2)),
+          totalAmount: Number(calculateDetails.totalAmount.toFixed(2)),
+          winningAmount: Number(calculateDetails.winningAmount.toFixed(2)),
+          netWinning: Number(
+            (
+              calculateDetails.winningAmount - calculateDetails.totalAmount
+            ).toFixed(2)
+          ),
+        };
+        const updatedTotalBetPlayed = betPlayed?.length
+          ? [...totalBetPlayed, updatedDetails]
+          : totalBetPlayed;
+        totalBetPlayed = updatedTotalBetPlayed;
+        betPlayed = [];
+        console.clear();
+        console.log("-----------");
+        console.log("Total Bets :", updatedTotalBetPlayed.length);
+        console.log(
+          "Total Amount Wagered :",
+          Number(
+            updatedTotalBetPlayed
+              .reduce((acc, bet) => acc + bet.totalAmount, 0)
+              .toFixed(2)
+          )
+        );
+        console.log(
+          "Total Winning Amount :",
+          Number(
+            updatedTotalBetPlayed
+              .reduce((acc, bet) => acc + bet.winningAmount, 0)
+              .toFixed(2)
+          )
+        );
+        console.log(
+          "Total Net Winning :",
+          Number(
+            updatedTotalBetPlayed
+              .reduce((acc, bet) => acc + bet.netWinning, 0)
+              .toFixed(2)
+          )
+        );
+        console.log("-----------");
+        console.log("Multiplier :", updatedDetails.multiplier);
+        console.log("------------");
+        console.log("Total Bets :", updatedDetails.totalBets);
+        console.log("Winning Bets :", updatedDetails.winningBets);
+        console.log("--------------");
+        console.log("Total Amount :", updatedDetails.totalAmount);
+        console.log("Winning Amount :", updatedDetails.winningAmount);
+        console.log("----------------");
+        console.log("Net Winning :", updatedDetails.netWinning);
+        console.log("-------------");
+      })
+      .catch(() => null)
+      .finally(() => resolve())
+  );
+};
+
+await executeBets();
+await fetchStats();
---
+++ PumpBet.js
@@ -0,0 +1,108 @@
+const generateRandomBet = ({ min, max, float }) => {
+  const randomNumber = Math.random() * (max - min) + min;
+  return float ? Number(randomNumber.toFixed(6)) : Math.ceil(randomNumber);
+};
+
+const generate = () => [
+  generateRandomBet({
+    min: 1,
+    max: [7, 9, 11, 13].includes(Math.floor(Math.random() * 15) + 1) ? 15 : 5,
+  }),
+  generateRandomBet({ min: 0.1, max: 0.5, float: true }),
+];
+
+const executeBets = async () => {
+  const [pump, amount] = generate();
+  const response = await fetch("https://stake.ac/_api/graphql", {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.7",
+      "content-type": "application/json",
+      priority: "u=1, i",
+      "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Brave";v="132"',
+      "sec-ch-ua-arch": '"x86"',
+      "sec-ch-ua-bitness": '"64"',
+      "sec-ch-ua-full-version-list":
+        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.0.0", "Brave";v="132.0.0.0"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-model": '""',
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-ch-ua-platform-version": '"10.0.0"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1",
+      "x-access-token":
+        "e7fd5123465a5169b13ece91f936ae89cd02bf54db1e8d345ad6e1cfe40402ed24be09c2edc085573eb9361284c23492",
+      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+    },
+    referrer: "https://stake.ac/casino/games/pump",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: JSON.stringify({
+      query:
+        "mutation PumpBet($amount: Float!, $difficulty: CasinoGamePumpDifficultyEnum!, $currency: CurrencyEnum!, $identifier: String, $round: Int!) {\n  pumpBet(\n    amount: $amount\n    difficulty: $difficulty\n    currency: $currency\n    identifier: $identifier\n    round: $round\n  ) {\n    ...CasinoBet\n    state {\n      ...CasinoGamePump\n    }\n  }\n}\n\nfragment CasinoBet on CasinoBet {\n  id\n  active\n  payoutMultiplier\n  amountMultiplier\n  amount\n  payout\n  updatedAt\n  currency\n  game\n  user {\n    id\n    name\n  }\n}\n\nfragment CasinoGamePump on CasinoGamePump {\n  difficulty\n  payoutMultiplier\n  round\n}\n",
+      variables: {
+        amount: amount,
+        currency: "inr",
+        identifier: "",
+        round: pump,
+        difficulty: "expert",
+      },
+    }),
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+  });
+  const data = await response.json();
+  const payout = data?.data?.pumpBet?.payout || 0;
+  return { pump, amount, payout, active: payout > 0 };
+};
+
+let betResponse = [];
+const printResult = (data) => {
+  const updatedBetResponse = [...betResponse, data];
+  const betsWin = updatedBetResponse.filter((bet) => bet.active).length;
+  const winRate = Number(
+    ((betsWin / updatedBetResponse.length) * 100).toFixed(2)
+  );
+  const totalAmount = Number(
+    updatedBetResponse?.reduce((acc, bet) => acc + bet.amount, 0).toFixed(2)
+  );
+  const winningAmount = Number(
+    updatedBetResponse
+      ?.reduce((acc, bet) => (bet.active ? acc + bet.payout : acc), 0)
+      .toFixed(2)
+  );
+  const netWinning = Number((winningAmount - totalAmount).toFixed(2));
+  console.clear();
+  console.log("--------------------");
+  console.log("Total Bets : ", updatedBetResponse.length);
+  console.log("--------------------");
+  console.log("Bets Win : ", betsWin);
+  console.log("Bets Lose : ", updatedBetResponse.length - betsWin);
+  console.log("Win Rate : ", winRate, "%");
+  console.log("--------------------");
+  console.log("Total Amount : ", totalAmount);
+  console.log("Winning Amount : ", winningAmount);
+  console.log("----------------");
+  console.log("Net Winning : ", netWinning);
+  console.log("-------------");
+  console.log("Recent Bet : ");
+  console.log("----Amount : ", Number(data.amount.toFixed(2)));
+  console.log("----Pump : ", data.pump);
+  console.log("----Result : ", data.active ? "Win" : "Lose");
+  betResponse = [...updatedBetResponse];
+  return netWinning;
+};
+
+let intervalRef = setInterval(async () => {
+  const data = await executeBets();
+  const netWinning = printResult(data);
+ if (netWinning < -100) {
+   clearInterval(intervalRef);
+ } else if (netWinning > 500) {
+   clearInterval(intervalRef);
+ }
+}, 750);
+
+clearInterval(intervalRef);
