d1b241a63ca4980cbea0138db1c76dd3b0dfc5da
Avinav Aery <aery1803@gmail.com>
2025-01-18 14:40:23 +0530

    minor fix

--- Dice.js
+++ Dice.js
@@ -1,3 +1,49 @@
+const headers = false
+  ? {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.7",
+      "content-type": "application/json",
+      priority: "u=1, i",
+      "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Brave";v="132"',
+      "sec-ch-ua-arch": '"x86"',
+      "sec-ch-ua-bitness": '"64"',
+      "sec-ch-ua-full-version-list":
+        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.0.0", "Brave";v="132.0.0.0"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-model": '""',
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-ch-ua-platform-version": '"10.0.0"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1",
+      "x-access-token":
+        "b2393fedd7d26472111f74195e5698981b6eea168bccb3cd03b8708e411aff0dab34bad0e442f07aabf983a090327dcd",
+      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+    }
+  : {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+      "content-type": "application/json",
+      priority: "u=1, i",
+      "sec-ch-ua":
+        '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+      "sec-ch-ua-arch": '"x86"',
+      "sec-ch-ua-bitness": '"64"',
+      "sec-ch-ua-full-version": '"132.0.6834.84"',
+      "sec-ch-ua-full-version-list":
+        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.6834.84", "Google Chrome";v="132.0.6834.84"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-model": '""',
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-ch-ua-platform-version": '"10.0.0"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-access-token":
+        "a43fa34a186cda70de42567e5d19ba49400b684038c41a78621db5aba60150de12b0a51b91f59e7f534eb07ca079dcf0",
+      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+    };
 const number = (number) => {
   return Number(number.toFixed(2));
 };
@@ -45,28 +91,7 @@ const executeBets = async () => {
     generateRandomBet({ min: 0, max: 2, float: false }) > 1 ? "below" : "above";
 
   const response = await fetch("https://stake.ac/_api/graphql", {
-    headers: {
-      accept: "*/*",
-      "accept-language": "en-US,en;q=0.9",
-      "content-type": "application/json",
-      priority: "u=1, i",
-      "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Brave";v="132"',
-      "sec-ch-ua-arch": '"x86"',
-      "sec-ch-ua-bitness": '"64"',
-      "sec-ch-ua-full-version-list":
-        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.0.0", "Brave";v="132.0.0.0"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-model": '""',
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-ch-ua-platform-version": '"10.0.0"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "sec-gpc": "1",
-      "x-access-token":
-        "e7fd5123465a5169b13ece91f936ae89cd02bf54db1e8d345ad6e1cfe40402ed24be09c2edc085573eb9361284c23492",
-      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
-    },
+    headers,
     referrer: "https://stake.ac/casino/games/dice",
     referrerPolicy: "strict-origin-when-cross-origin",
     body: JSON.stringify({
--- Pump.js
+++ Pump.js
@@ -1,3 +1,44 @@
+const headers = false
+  ? {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.7",
+      "content-type": "application/json",
+      priority: "u=1, i",
+      "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Brave";v="132"',
+      "sec-ch-ua-arch": '"x86"',
+      "sec-ch-ua-bitness": '"64"',
+      "sec-ch-ua-full-version-list":
+        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.0.0", "Brave";v="132.0.0.0"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-model": '""',
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-ch-ua-platform-version": '"10.0.0"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1",
+      "x-access-token":
+        "b2393fedd7d26472111f74195e5698981b6eea168bccb3cd03b8708e411aff0dab34bad0e442f07aabf983a090327dcd",
+      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+    }
+  : {
+      "content-type": "application/json",
+      "sec-ch-ua":
+        '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+      "sec-ch-ua-arch": '"x86"',
+      "sec-ch-ua-bitness": '"64"',
+      "sec-ch-ua-full-version": '"132.0.6834.84"',
+      "sec-ch-ua-full-version-list":
+        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.6834.84", "Google Chrome";v="132.0.6834.84"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-model": '""',
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-ch-ua-platform-version": '"10.0.0"',
+      "x-access-token":
+        "a43fa34a186cda70de42567e5d19ba49400b684038c41a78621db5aba60150de12b0a51b91f59e7f534eb07ca079dcf0",
+      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
+    };
+
 const number = (number) => {
   return Number(number.toFixed(2));
 };
@@ -64,35 +105,14 @@ const executeBets = async () => {
       : generateRandomBet({ min: 0.4, max: 0.99 });
   const mode = generateRandomBet({
     min: 1,
-    max: pump > 10 ? 4 : 3,
+    max: 4,
     float: false,
   });
 
   const difficulty = ["easy", "medium", "hard", "expert"][mode - 1];
 
   const response = await fetch("https://stake.ac/_api/graphql", {
-    headers: {
-      accept: "*/*",
-      "accept-language": "en-US,en;q=0.7",
-      "content-type": "application/json",
-      priority: "u=1, i",
-      "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Brave";v="132"',
-      "sec-ch-ua-arch": '"x86"',
-      "sec-ch-ua-bitness": '"64"',
-      "sec-ch-ua-full-version-list":
-        '"Not A(Brand";v="8.0.0.0", "Chromium";v="132.0.0.0", "Brave";v="132.0.0.0"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-model": '""',
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-ch-ua-platform-version": '"10.0.0"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "sec-gpc": "1",
-      "x-access-token":
-        "e7fd5123465a5169b13ece91f936ae89cd02bf54db1e8d345ad6e1cfe40402ed24be09c2edc085573eb9361284c23492",
-      "x-lockdown-token": "s5MNWtjTM5TvCMkAzxov",
-    },
+    headers,
     referrer: "https://stake.ac/casino/games/pump",
     referrerPolicy: "strict-origin-when-cross-origin",
     body: JSON.stringify({
@@ -119,6 +139,7 @@ const executeBets = async () => {
     target: payoutMultiplier,
     amount,
     payout,
+    pump,
     active: payout > 0,
     difficulty,
   };
@@ -138,7 +159,7 @@ let highestBet = {
   payout: 0,
 };
 
-const printResult = ({ active, payout, amount, target, difficulty }) => {
+const printResult = ({ active, payout, amount, target, difficulty, pump }) => {
   const totalBets = betDetails.totalBets + 1;
   const betsWin = betDetails.betsWin + (active ? 1 : 0);
   const betsLose = totalBets - betsWin;
@@ -172,6 +193,7 @@ const printResult = ({ active, payout, amount, target, difficulty }) => {
   console.log("----Amount : ", number(amount));
   console.log("----Winning : ", number(payout));
   console.log("----Target : ", target);
+  console.log("----Pump : ", pump);
   console.log("----Difficulty : ", difficulty);
   console.log("----Result : ", active ? "Win" : "Lose");
   console.log("-------------");
