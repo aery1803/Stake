2eb3d0b5af560c28ae955a02663d4ea82d483ef8
Avinav Aery <aery1803@gmail.com>
2025-04-13 21:05:06 +0530

    minor fix

--- Slider.js
+++ Slider.js
@@ -70,17 +70,18 @@ const generateRandomArrays = ({ length, min = 1.01, max }) => {
 };
 
 const multipliers = [
-  ...generateRandomArrays({ length: 10, max: 500000 }),
-  ...generateRandomArrays({ length: 15, max: 3000 }),
-  ...generateRandomArrays({ length: 20, max: 100 }),
-  ...generateRandomArrays({ length: 45, max: 20 }),
-  ...generateRandomArrays({ length: 100, max: 5 }),
   ...generateRandomArrays({ length: 160, max: 2 }),
+  ...generateRandomArrays({ length: 100, max: 4 }),
+  ...generateRandomArrays({ length: 40, max: 10 }),
+  ...generateRandomArrays({ length: 50, max: 20 }),
+  // ...generateRandomArrays({ length: 20, max: 100 }),
+  // ...generateRandomArrays({ length: 15, max: 3000 }),
+  // ...generateRandomArrays({ length: 10, max: 100000 }),
 ];
 
 const amountPerBet = [
-  ...generateRandomArrays({ length: 100, min: 0.05, max: 0.25 }),
-  ...generateRandomArrays({ length: 250, min: 0.1, max: 0.5 }),
+  ...generateRandomArrays({ length: 250, min: 0.15, max: 0.4 }),
+  ...generateRandomArrays({ length: 100, min: 0.05, max: 0.15 }),
 ];
 
 let betPlayed = [];
@@ -134,29 +135,7 @@ const executeBets = async () => {
 const fetchStats = async () => {
   await new Promise((resolve) =>
     fetch("https://stake.ac/_api/graphql", {
-      headers: {
-        accept: "application/graphql+json, application/json",
-        "accept-language": "en-US,en;q=0.5",
-        "content-type": "application/json",
-        priority: "u=1, i",
-        "sec-ch-ua":
-          '"Brave";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-        "sec-ch-ua-arch": '"x86"',
-        "sec-ch-ua-bitness": '"64"',
-        "sec-ch-ua-full-version-list":
-          '"Brave";v="131.0.0.0", "Chromium";v="131.0.0.0", "Not_A Brand";v="24.0.0.0"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-model": '""',
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-ch-ua-platform-version": '"10.0.0"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "sec-gpc": "1",
-        "x-access-token":
-          "4ef5ed7fb67e30e1050e24779114cfdbd52cda62d922e1cd08de4b6a34d77349e1b95b2b53a3796e12fefa0cf79509b7",
-        "x-language": "en",
-      },
+      headers,
       referrer: "https://stake.ac/casino/games/slide",
       referrerPolicy: "strict-origin-when-cross-origin",
       method: "POST",
@@ -189,13 +168,11 @@ const fetchStats = async () => {
         };
         const updatedDetails = {
           ...calculateDetails,
-          multiplier: Number(calculateDetails.multiplier.toFixed(2)),
-          totalAmount: Number(calculateDetails.totalAmount.toFixed(2)),
-          winningAmount: Number(calculateDetails.winningAmount.toFixed(2)),
-          netWinning: Number(
-            (
-              calculateDetails.winningAmount - calculateDetails.totalAmount
-            ).toFixed(2)
+          multiplier: number(calculateDetails.multiplier),
+          totalAmount: number(calculateDetails.totalAmount),
+          winningAmount: number(calculateDetails.winningAmount),
+          netWinning: number(
+            calculateDetails.winningAmount - calculateDetails.totalAmount
           ),
         };
         const updatedTotalBetPlayed = betPlayed?.length
@@ -208,26 +185,23 @@ const fetchStats = async () => {
         console.log("Total Bets :", updatedTotalBetPlayed.length);
         console.log(
           "Total Amount Wagered :",
-          Number(
-            updatedTotalBetPlayed
-              .reduce((acc, bet) => acc + bet.totalAmount, 0)
-              .toFixed(2)
+          number(
+            updatedTotalBetPlayed.reduce((acc, bet) => acc + bet.totalAmount, 0)
           )
         );
         console.log(
           "Total Winning Amount :",
-          Number(
-            updatedTotalBetPlayed
-              .reduce((acc, bet) => acc + bet.winningAmount, 0)
-              .toFixed(2)
+          number(
+            updatedTotalBetPlayed.reduce(
+              (acc, bet) => acc + bet.winningAmount,
+              0
+            )
           )
         );
         console.log(
           "Total Net Winning :",
-          Number(
-            updatedTotalBetPlayed
-              .reduce((acc, bet) => acc + bet.netWinning, 0)
-              .toFixed(2)
+          number(
+            updatedTotalBetPlayed.reduce((acc, bet) => acc + bet.netWinning, 0)
           )
         );
         console.log("-----------");
@@ -251,6 +225,15 @@ let interval;
 let betDone = false;
 let prevButtonText = null;
 
+const number = (number) => {
+  return Number(number.toFixed(2));
+};
+
+const generateRandomBet = ({ min, max, float = true }) => {
+  const randomNumber = Math.random() * (max - min) + min;
+  return float ? number(randomNumber) : Math.ceil(randomNumber);
+};
+
 const executeCode = () => {
   interval = setInterval(() => {
     const data = document
@@ -259,7 +242,8 @@ const executeCode = () => {
       .trim()
       .toLowerCase();
     if (data === "bet" && prevButtonText !== data) {
-      executeBets();
+      const randomNumber = generateRandomBet({ min: 1, max: 10, float: false });
+      if (randomNumber > 5) executeBets();
       prevButtonText = data;
     } else if (data === "bet (next round)" && prevButtonText !== data) {
       fetchStats();
@@ -269,3 +253,4 @@ const executeCode = () => {
 };
 
 clearInterval(interval);
+executeCode();
